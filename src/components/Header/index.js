2809afb2149034266cb41c83c935c99240969edf
Bleyle <Bennybleylle09@gmail.com>
2022-10-19 15:00:53 +0300

    Add files via upload

---
+++ src/components/Header/index.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import cn from "classnames";
+import styles from "./Header.module.sass";
+import Icon from "../Icon";
+import Image from "../Image";
+import Notification from "./Notification";
+import User from "./User";
+
+const nav = [
+  {
+    url: "/search01",
+    title: "Twitter",
+  },
+  {
+    url: "/faq",
+    title: "Medium",
+  },
+  {
+    url: "/item",
+    title: "Docs",
+  },
+  {
+    url: "/profile",
+    title: "Profile",
+  },
+];
+
+const Headers = () => {
+  const [visibleNav, setVisibleNav] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const handleSubmit = (e) => {
+    alert();
+  };
+
+  return (
+    <header className={styles.header}>
+      <div className={cn("container", styles.container)}>
+        <Link className={styles.logo} to="/">
+          <Image
+            className={styles.pic}
+            src="/images/logo-dark.png"
+            srcDark="/images/logo-light.png"
+            alt="Fitness Pro"
+          />
+        </Link>
+        <div className={cn(styles.wrapper, { [styles.active]: visibleNav })}>
+          <nav className={styles.nav}>
+            {nav.map((x, index) => (
+              <Link
+                className={styles.link}
+                // activeClassName={styles.active}
+                to={x.url}
+                key={index}
+              >
+                {x.title}
+                
+              </Link>
+            ))}
+          </nav>
+          <form
+            className={styles.search}
+            action=""
+            onSubmit={() => handleSubmit()}
+          >
+        
+            
+          </form>
+          <Link
+            className={cn("button-small", styles.button)}
+            to="/upload-variants"
+          >
+            Upload
+          </Link>
+        </div>
+        <Notification className={styles.notification} />
+        <Link
+          className={cn("button-small", styles.button)}
+          to="/upload-variants"
+        >
+          Paper Trading
+        </Link>
+        {/* <Link
+          className={cn("button-stroke button-small", styles.button)}
+          to="/connect-wallet"
+        >
+          Connect Wallet
+        </Link> */}
+        
+        
+      </div>
+    </header>
+  );
+};
+
+export default Headers;
